de76d94679683b4a66c2c0d14600098ce516471b
ShardulA11 <104989321+ShardulA11@users.noreply.github.com>
2022-07-16 19:51:07 +0530

    Update Project 8.R

--- Project 8.R
+++ Project 8.R
@@ -1,11 +1,40 @@
 # Simple Calculator
-val1 = 5
-val2 = 6
-val3 = "add"
-result = switch(
-val3,
-"add" = cat("Addition = ", val1+val2),
-"sub" = cat("Subtraction = ", val1-val2), 
-"div" = cat("Division = ", val1/val2),
-"mult" = cat("Multiplication = ", val1*val2)
-)
+> val1 = 5
+> val2 = 6
+> val3 = "s"
+> result = switch(
++     val3,
++     "add" = cat("Addition = ", val1+val2),
++     "sub" = cat("Subtraction = ", val1-val2), 
++     "div" = cat("Division = ", val1/val2),
++     "mult" = cat("Multiplication = ", val1*val2)
++ )
+> print(result)
+NULL
+> # Simple Calculator
+> val1 = 5
+> val2 = 6
+> val3 = "mult"
+> result = switch(
++     val3,
++     "add" = cat("Addition = ", val1+val2),
++     "sub" = cat("Subtraction = ", val1-val2), 
++     "div" = cat("Division = ", val1/val2),
++     "mult" = cat("Multiplication = ", val1*val2)
++ )
+Multiplication =  30> 
+> print(result)
+NULL
+> # Simple Calculator
+> val1 = 5
+> val2 = 6
+> val3 = "add"
+> result = switch(
++     val3,
++     "add" = cat("Addition = ", val1+val2),
++     "sub" = cat("Subtraction = ", val1-val2), 
++     "div" = cat("Division = ", val1/val2),
++     "mult" = cat("Multiplication = ", val1*val2)
++ )
+Addition =  11> print(result)
+NULL
